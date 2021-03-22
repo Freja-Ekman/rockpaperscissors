f52ea0422cecc4ff302fb408ff0e3974e803b0a5
FrejaEkman <freja@sedata.org>
2021-03-22 19:26:03 +0100

    Fix error handling for move, catch IllegalStateException for several moves

--- src/main/java/se/cygni/rockpaperscissors/api/RockPaperScissorsController.java
+++ src/main/java/se/cygni/rockpaperscissors/api/RockPaperScissorsController.java
@@ -10,6 +10,7 @@ import se.cygni.rockpaperscissors.api.model.StatusDto;
 import se.cygni.rockpaperscissors.application.model.Game;
 import se.cygni.rockpaperscissors.application.model.GameMove;
 import se.cygni.rockpaperscissors.application.model.Move;
+import se.cygni.rockpaperscissors.application.model.Player;
 import se.cygni.rockpaperscissors.application.services.GameService;
 
 import java.util.UUID;
@@ -90,21 +91,23 @@ public class RockPaperScissorsController {
     public void postMove(@PathVariable("id") String id, @RequestBody MoveDto moveDto) {
         try {
             Game game = gameService.getGame(UUID.fromString(id));
-            if(game == null)
+            if (game == null)
                 throw new ResponseStatusException(NOT_FOUND, "Game does not exist");
-            GameMove move = moveDto.toDomain();
+            GameMove gameMove = moveDto.toDomain();
             try {
-                game.getGameMove(move.getPlayer()).setMove(move.getMove());
+                Player player = gameMove.getPlayer();
+                    try {
+                        game.getGameMove(player).setMove(gameMove.getMove());
+                    } catch (IllegalArgumentException e) {
+                        throw new ResponseStatusException(BAD_REQUEST, "Can not make move hidden");
+                    } catch (IllegalStateException e) {
+                        throw new ResponseStatusException(PRECONDITION_FAILED, "Already moved");
+                    }
             } catch (IllegalStateException e) {
                 throw new ResponseStatusException(NOT_FOUND, "Player does not exist");
-            } catch (IllegalArgumentException e) {
-                throw new ResponseStatusException(BAD_REQUEST, "Can not make move hidden");
             }
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(BAD_REQUEST, "Wrong id format");
-        } catch (IllegalStateException e) {
-            throw new ResponseStatusException(PRECONDITION_FAILED, "Already moved");
         }
-
     }
 }
